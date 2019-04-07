8c5767cc21c42802ee77beaa59b054020aed369b
Mehul Prajapati <mehulprajapati2802@gmail.com>
2019-04-06 22:01:41 -0700

    move files

--- addition.sh
+++ addition.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-echo 'Enter the First Number :'
+echo 'Enter the 1st no:'
 read a
-echo 'Enter the Second Number :'
+
+echo 'Enter the 2nd no:'
 read b
+
 x=$(expr "$a" + "$b")
 echo $a + $b = $x
